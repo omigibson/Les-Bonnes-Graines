2b116cf2d618e7a1c6d24ab5170aa5832b0b3809
Omi Gibba <oumie.gibba@gmail.com>
2020-08-04 14:08:10 -0700

    Create sass variables. Style single course page (desktop).

--- wp-content/themes/lesbonnesgraines/single.php
+++ wp-content/themes/lesbonnesgraines/single.php
@@ -3,18 +3,19 @@
    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
          <header>
-            <h1 class="entry-title">
+            <h2 class="entry-title">
                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
-            </h1>
+            </h2>
          </header>
          <div class="entry-content">
-            <div class="course-info">
+            <div class="image-and-info">
                <?php if ( has_post_thumbnail() ) : the_post_thumbnail(); endif; ?>
-               <div>
+               <div class="course-info">
+                  <h3>Om kursen</h3>
                   <ul>
                      <li>
-                     <?php if ( get_field('start_date') ) : ?>
-                           <span><?php the_field("start_date") ?></span>
+                     <?php if ( get_field("start_date") ) : ?>
+                           <span>Datum: <?php the_field("start_date") ?></span>
                         <?php if ( get_field("end_date") ) : ?>
                            <span> -  <?php the_field("end_date") ?></span>
                         <?php
@@ -22,16 +23,16 @@
                         endif; ?>
                      </li>
                      <li>
-                        <?php if ( get_field('price') ) : the_field("price"); endif; ?>
+                        <?php if ( get_field("price") ) : echo "Pris: €" . get_field("price"); endif; ?>
                      </li>
                      <li>
-                        <?php if ( get_field('location') ) : the_field("location"); endif; ?>
+                        <?php if ( get_field("location") ) : echo "Plats: " . get_field("location"); endif; ?>
                      </li>
                   </ul>
+                  <p><?php if ( get_field("additional_info") ) : the_field("additional_info"); endif; ?></p>
                </div>
             </div>
-            <?php the_content(); ?>
-            <div class="entry-links"><?php wp_link_pages(); ?></div>
+            <div class="post-content"><?php the_content(); ?></div>
          </div>
       </article>
    <?php endwhile; endif; ?>
